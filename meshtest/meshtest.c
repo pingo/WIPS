815dd375b54ee730a9eb64eec2d97e950e255160
Max Morén <maxmoren@gmail.com>
2011-04-13 15:48:12 +0200

    Added some changes I missed.

--- meshtest/meshtest.c
+++ meshtest/meshtest.c
@@ -1,6 +1,7 @@
 #include "contiki.h"
 #include "net/rime.h"
 #include "net/rime/mesh.h"
+#include "net/mac/mac.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
 
@@ -23,12 +24,8 @@ static void timedout(struct mesh_conn *c)
 
 static void recv(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
 {
-	printf("Data received from %d.%d: %.*s (%d).\n",
-		from->u8[0], from->u8[1],
-		packetbuf_datalen(), (char *)packetbuf_dataptr(), packetbuf_datalen());
-
-	packetbuf_copyfrom("Hopp", 4);
-	mesh_send(&mesh, from);
+	printf("Data received from %d.%d (%d B), %d hops.\n",
+		from->u8[0], from->u8[1], packetbuf_datalen(), hops);
 }
 
 const static struct mesh_callbacks callbacks = {recv, sent, timedout};
@@ -40,22 +37,24 @@ PROCESS_THREAD(meshtest_process, ev, data)
 
 	mesh_open(&mesh, 132, &callbacks);
 
-	SENSORS_ACTIVATE(button_sensor);
+	uint8_t occupied = 0;
 
-/*	printf("Started.\n"); */
+	SENSORS_ACTIVATE(button_sensor);
 
 	for (;;)
 	{
-		rimeaddr_t addr;
 
 		PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
 
-		leds_toggle(LEDS_GREEN);
-		/* printf("Click.\n"); */
+		occupied = !occupied;
+		printf("Room now %s\n", occupied ? "occupied" : "unoccupied");
 
-		packetbuf_copyfrom("Hej", 3);
-		addr.u8[0] = 1;
-		addr.u8[1] = 0;
+		if (occupied) leds_on(LEDS_GREEN);
+		else          leds_off(LEDS_GREEN);
+
+		rimeaddr_t addr = { { 61, 0 } };
+	
+		packetbuf_copyfrom(&occupied, 1);
 		mesh_send(&mesh, &addr);
 	}
 
