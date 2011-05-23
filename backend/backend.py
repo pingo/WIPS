a7ffbaaf37c29b9c82bd6ff6c3143c8bce983372
Max Morén <maxmoren@gmail.com>
2011-05-23 14:10:48 +0200

    Added XMLRPC server to backend so that frontend can execute methods on it. Removed spaces from sink output format.

--- backend/backend.py
+++ backend/backend.py
@@ -1,35 +1,153 @@
 import serial, argparse, sqlite3, csv
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from threading import Thread, Lock, Event
+from time import gmtime
+
+s       = None    # Serial port file object.
+q       = {}      # Queued ACKs.
+qLock   = Lock()  # Lock for ACK queue.
+uEvent  = Event() # New data event.
+uLatest = 0;
+
+UPDATE_TIMEOUT = 5.0  # How long to wait for ACKs.
+DATA_TIMEOUT = 10.0   # How long to block when waiting for new data.
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+def queueAck(addr, t):
+	acksLock.acquire()
+	l = acks.setdefault(addr, [])
+	l.append(Ack(t))
+	acksLock.release()
+
+def updateValue(t, addr, value):
+	s.write(str(addr) + ':' + t + str(value) + '\r\n')
+
+	qLock.acquire()
+	acks = q.setdefault(addr, {})
+
+	# If the delta is already being set by somebody else, we fail.
+	if t in acks:
+		qLock.release()
+		return False
+
+	# Create a new event to wait for.
+	event = Event()
+
+	acks[t] = event
+	qLock.release()
+
+	success = event.wait(UPDATE_TIMEOUT)
+
+	qLock.acquire()
+	del acks[t]
+	qLock.release()
+
+	return success
+
+
+class ServerThread(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+
+	def rpcSetDelta(self, addr, value):
+		return updateValue('d', addr, value)
+
+	def rpcSetSensorTimeout(self, addr, value):
+		return updateValue('s', addr, value)
+
+	def rpcSetSensorInterval(self, addr, value):
+		return updateValue('i', addr, value)
+
+	def rpcSetBeaconInterval(self, addr, value):
+		return updateValue('b', value)
+
+	def rpcWaitForData(self, time):
+		global uEvent, uLatest
+
+		if uLatest > time:
+			return true
+
+		success = uEvent.wait(DATA_TIMEOUT)
+		uEvent.clear()
+		return success
+
+	def run(self):
+		print "Starting server..."
+
+		self.server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
+		self.server.register_introspection_functions()
+		self.server.register_multicall_functions()
+		self.server.register_function(self.rpcSetDelta, 'setDelta')
+		self.server.register_function(self.rpcSetSensorTimeout, 'setSensorTimeout')
+		self.server.register_function(self.rpcSetSensorInterval, 'setSensorInterval')
+		self.server.register_function(self.rpcSetBeaconInterval, 'setBeaconInterval')
+		self.server.register_function(self.rpcWaitForData, 'waitForData')
+
+		self.server.serve_forever()
+
+server = ServerThread()
+server.start()
 
 def serve(db, port):
+	global s
+
 	s = serial.Serial(port, 115200)
-	s.readline() # Skip one line to prevent fragmented line.
+	# Skip one line to prevent fragmented line.
+	s.readline() 
 
 	reader = csv.reader(s, delimiter=',')
 
 	for line in reader:
-		addr, hops, rssi, seq_flag, retries, v = line
+		if line[0] == 't':
+			typ, addr, hops, rssi, seq_flag, retries, v = line
 
-		major, minor = addr.split('.')
+			major, minor = addr.split('.')
 
-		t = (
-				int(major),
-				int(minor)
-			)
+			t = (
+					int(major),
+					int(minor)
+				)
 
-		db.execute('INSERT OR IGNORE INTO `mote` (`major`, `minor`) VALUES (?, ?)', t)
+			db.execute('INSERT OR IGNORE INTO `mote` (`major`, `minor`) VALUES (?, ?)', t)
 
-		t = (
-				int(major),
-				int(minor),
-				int(hops),
-				int(rssi),
-				int(seq_flag),
-				int(retries),
-				int(v)
-			)
+			t = (
+					int(major),
+					int(minor),
+					int(hops),
+					int(rssi),
+					int(seq_flag),
+					int(retries),
+					int(v)
+				)
 
-		db.execute('INSERT INTO `event` VALUES (?, ?, strftime(\'%s\', \'now\'), ?, ?, ?, ?, ?)', t)
-		db.commit()
+			db.execute('INSERT INTO `event` VALUES (?, ?, strftime(\'%s\', \'now\'), ?, ?, ?, ?, ?)', t)
+			db.commit()
+
+			print line
+
+			uLatest = gmtime()
+			uEvent.set() # New data has arrived.
+
+		else:
+			typ = line[0]
+			addr = line[1]
+
+			print 'Recieved ACK type', typ, 'from', addr
+
+			qLock.acquire()
+
+			if addr in q:
+				e = q[addr]
+				e[typ].set()
+
+				print 'Success'
+			else:
+				print 'Unqueued ACK received from', addr
+
+			qLock.release()
 
 def main():
 	argumentparser = argparse.ArgumentParser(description='Presence sensing storage server')
--- sink/callbacks.c
+++ sink/callbacks.c
@@ -15,19 +15,53 @@ void cb_recv(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
 	char *ptr = packetbuf_dataptr();
 	int seq_flag, retries, payload;
 	char buf[2];
+	char type;
 
-	proto_status_unpack(ptr, &seq_flag, &retries, &payload);
+	switch (proto_p_type(ptr))
+	{
+		case P_TYPE_STATUS:
+			proto_status_unpack(ptr, &seq_flag, &retries, &payload);
 
-	printf("%d.%d, %2u, %4d, %d, %3d, %d\n",
-		from->u8[0], from->u8[1],
-		hops,
-		packetbuf_attr(PACKETBUF_ATTR_RSSI),
-		seq_flag, retries, payload);
+			printf("t,%d.%d,%u,%d,%d,%d,%d\n",
+				from->u8[0], from->u8[1],
+				hops,
+				packetbuf_attr(PACKETBUF_ATTR_RSSI),
+				seq_flag, retries, payload);
 
-	proto_ack_pack(buf, seq_flag, retries);
+			type = 't';
 
-	packetbuf_copyfrom(buf, 2);
-	mesh_send(&mesh, from);
+			proto_ack_pack(buf, seq_flag, retries);
+
+			packetbuf_copyfrom(buf, 2);
+			mesh_send(&mesh, from);
+			break;
+
+		case P_TYPE_SET_DELTA:
+			type = 'd';
+			break;
+
+		case P_TYPE_SET_SENSOR_INTERVAL:
+			type = 'i';
+			break;
+
+		case P_TYPE_SET_SENSOR_TIMEOUT:
+			type = 's';
+			break;
+
+		case P_TYPE_SET_BEACON_INTERVAL:
+			type = 'b';
+			break;
+
+		default:
+			return;
+	}
+
+	if (type != 't')
+	{
+		printf("%c,%d.%d\n",
+			type,
+			from->u8[0], from->u8[1]);
+	}
 }
 
 void cb_sent(struct mesh_conn *c)
